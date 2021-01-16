2d7e180ad158a8b476ae3e56aabe765779d02de0
Olesya Schukina <olesyaschu@yandex.ru>
2021-01-16 20:07:55 +0300

    Modify pages/index.vue and layouts/default.vue: replace spaces with tabs

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,13 @@
 <template>
-  <div>
-    <Header />
-    <div class="catalog">
-      <h1>Каталог</h1>
-      <Sorting />
-      <Categories />
-    </div>
-    <Nuxt />
-  </div>
+	<div>
+		<Header />
+		<div class="catalog">
+			<h1>Каталог</h1>
+			<Sorting />
+			<Categories />
+		</div>
+		<Nuxt />
+	</div>
 </template>
 
 <style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,27 @@
 <template>
-  <div>
-    <ol>
-      <Product 
-      v-for="product in productList" 
-      v-bind:key="product.id" 
-      v-bind:Product="product"
-      />
-    </ol>
-  </div>
+	<div>
+		<ol>
+			<Product 
+			v-for="product in productList" 
+			v-bind:key="product.id" 
+			v-bind:Product="product"
+			/>
+		</ol>
+	</div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      productList: [],
-    }
-  },
-  mounted: function () {
-    fetch('https://frontend-test.idaproject.com/api/product?category=1')
-      .then(response => response.json())
-      .then(commits => (this.productList = commits));
-  },
+	data() {
+		return {
+			productList: [],
+		}
+	},
+	mounted: function () {
+		fetch('https://frontend-test.idaproject.com/api/product?category=1')
+			.then(response => response.json())
+			.then(commits => (this.productList = commits));
+	},
 }
 </script>
 
