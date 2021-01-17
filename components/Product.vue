688804d2f102b0dfcbbe80d8048e14acd9efcf84
Olesya Schukina <olesyaschu@yandex.ru>
2021-01-18 02:09:37 +0300

    Modify components/Product.vue: add filtration of names and prices from API

--- components/Product.vue
+++ components/Product.vue
@@ -19,6 +19,10 @@ export default {
 	data () {
 		return {
 			photo: 'https://frontend-test.idaproject.com' + this.Product.photo,
+			name: this.Product.name.toLowerCase(),
+			//отделяет три числа пробелом в цене, работает с любой разрядностью
+			//делает число строкой, переворачивает ее, добавляет к каждой тройке чисел пробел, переворачивает строку обратно и убирает лишний пробел, если он есть
+			price: (this.Product.price + '').split('').reverse().join('').replace(/\d{3}/g, match => match + ' ').split('').reverse().join('').trim() + ' ₽',
 		};
 	},
 }
