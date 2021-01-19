365020b01c7957ca2fcc70cb6bc3aee9f194c3c2
Olesya Schukina <olesyaschu@yandex.ru>
2021-01-20 02:05:20 +0300

    Add image assets/img/close.png, components Cart, CartContent, ProductInCart and modify component CartIcon: add components for cart

---
+++ components/Cart.vue
@@ -0,0 +1,65 @@
+<template>
+	<div>
+		<div 
+		v-on:click="$emit('closeCart')"
+		class="fade"></div>
+		<div class="cart">
+			<div class="cart__header">
+				<h1 class="title title_size_xl">Корзина</h1>
+				<img 
+				v-on:click="$emit('closeCart')"
+				class="cart__exit" 
+				src="~/assets/img/close.png">
+			</div>
+			<CartContent 
+			v-on:closeCart="$emit('closeCart')"/>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	data () {
+		return {
+
+		}
+	}
+}
+</script>
+
+<style lang="scss">
+	.fade {
+		position: fixed;
+		top: 0;
+		left: 0;
+		height: 100vh;
+		width: 100vw;
+		z-index: 999;
+		background: #FFFFFF;
+		opacity: 0.8;
+	}
+
+	.cart {
+		height: 100vh;
+		width: 460px;
+		position: fixed;
+		top: 0;
+		right: 240px;
+		background: #FFFFFF;
+		box-shadow: -4px 0px 16px rgba(0, 0, 0, 0.05);
+		border-radius: 8px 0px 0px 8px;
+		z-index: 1000;
+		padding: 0 48px;
+
+		&__header {
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			margin-top: 50px;
+		}
+
+		&__exit {
+			cursor: pointer;
+		}
+	}
+</style>
---
+++ components/CartContent.vue
@@ -0,0 +1,60 @@
+<template>
+	<div>
+		<div v-if="cartIsEmpty">
+			<p class="title title_weight_normal title_size_l cart__title">
+				Пока что вы ничего не добавили в корзину.
+			</p>
+			<button 
+			v-on:click="$emit('closeCart')"
+			class="title title_weight_normal title_size_m cart__button">
+				Перейти к выбору
+			</button>
+		</div>
+		<div v-else>
+			<p class="title title_weight_normal cart__p">
+				Товары в корзине
+			</p>
+			<ol>
+				<ProductInCart />
+			</ol>
+			<p class="title title_weight_normal cart__p">
+				Оформить заказ
+			</p>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	data () {
+		return {
+			cartIsEmpty: true,
+		}
+	},
+}
+</script>
+
+<style lang="scss">
+@import '~/assets/styles/mixins';
+	.cart {
+		&__title {
+			width: 90%;
+			margin: 24px 0;
+		}
+
+		&__button {
+			width: 100%;
+			height: 50px;
+			background: #1F1F1F;
+			border-radius: 8px;
+			color: white;
+
+			@include change-color-on-hover(background,#1F1F1F, #59606D);
+		}
+
+		&__p {
+			font-size: 18px;
+			line-height: 23px;
+		}
+	}
+</style>
